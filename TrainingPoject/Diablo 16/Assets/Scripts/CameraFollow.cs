908ee9cf7f98e0a53b0a890d5be30f43625a4639
plamenivanov91 <mrplamenivanov91@gmail.com>
2016-03-18 18:08:03 +0200

    Added functuallity

--- TrainingPoject/Diablo 16/Assets/Scripts/CameraFollow.cs
+++ TrainingPoject/Diablo 16/Assets/Scripts/CameraFollow.cs
@@ -4,15 +4,47 @@ using System.Collections;
 public class CameraFollow : MonoBehaviour {
 
     public GameObject player;
-
+	private Rigidbody playerRigidbody;
     private Vector3 offset;
+	private Vector3 desiredPosition;
+	public GameObject cubeObj;
+	private bool moveTheFaggot = false;
 
-	// Use this for initialization
 	void Start () {
+		
         offset = transform.position - player.transform.position;
+		playerRigidbody = player.GetComponent<Rigidbody> ();
+
 	}
-	
-	// Update is called once per frame
+
+	void Update(){
+		if (Input.GetMouseButton(1)) {
+			RaycastHit hit;
+			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+			if (Physics.Raycast(ray, out hit)) {
+				if (hit.collider != null && !hit.collider.CompareTag("Player")) {
+					player.transform.LookAt (hit.point);
+					desiredPosition = hit.point;
+					moveTheFaggot = true;
+				}
+			}
+		}
+	}
+
+	void FixedUpdate(){
+		
+		if (moveTheFaggot) {
+			if (player.transform.position == desiredPosition) {
+				Debug.Log ("Arrived at point");
+				moveTheFaggot = false;
+			} else {
+				Vector3 kovra = new Vector3 (desiredPosition.x - player.transform.position.x, 0f, desiredPosition.z - player.transform.position.z);
+				player.transform.position += (kovra * 0.1f);
+			}
+		}
+
+	}
+
 	void LateUpdate () {
         transform.position = player.transform.position + offset;
 	}
